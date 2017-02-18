05ef1bce448248d07530e07d322d1b4e0966805a
Mark Lescroart <marklescroart@users.noreply.github.com>
2017-02-17 16:03:41 -0800

    Updated data file
    Data file has been updated to shrink file size for three files.

--- disks/cogneuro.bash
+++ disks/cogneuro.bash
@@ -2,6 +2,6 @@
 
 set -e
 
-wget https://www.dropbox.com/s/qyd48wpdxc6x1xo/cogneuro88data.tar.gz
+wget https://www.dropbox.com/s/mkr9vcwxn7drbcg/cogneuro88data.tar.gz
 tar -xvf cogneuro88data.tar.gz
 rm cogneuro88data.tar.gz
