e32042cdc3f3cf63bd284c969911b6a84ab39f86
Mark Lescroart <marklescroart@users.noreply.github.com>
2017-02-26 23:28:20 -0800

    Update cogneuro data
    Added (temporary?) EEG data to shared cogneuro folder.

--- disks/cogneuro.bash
+++ disks/cogneuro.bash
@@ -2,6 +2,7 @@
 
 set -e
 
-wget https://www.dropbox.com/s/qyd48wpdxc6x1xo/cogneuro88data.tar.gz
+# Temporary EEG data in this file; may be replaced by subsequent pull request
+wget https://www.dropbox.com/s/knjs0o8lkemzecj/cogneuro88data.tar.gz
 tar -xvf cogneuro88data.tar.gz
 rm cogneuro88data.tar.gz
